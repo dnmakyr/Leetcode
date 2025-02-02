962ca1be094cfa45ea6584613eb317fc96fac6a9
Le Dai Nguyen <128657481+dnmakyr@users.noreply.github.com>
2025-02-03 01:28:37 +0700

    feat: solved function composition

---
+++ function composition/solution.js
@@ -0,0 +1,14 @@
+function compose(functions) {
+    return function (x) {
+        var res = x;
+        for (var i = functions.length - 1; i >= 0; i--) {
+            res = functions[i](res);
+        }
+        return res;
+    };
+}
+;
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */ 
